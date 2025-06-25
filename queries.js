f2f7d2e666a0984c79fd90fe2fe2aa0873210f91
MOSES GACHUGU <mosesgachugu5@gmail.com>
2025-06-25 10:31:03 +0300

    queries.js

--- queries.js
+++ queries.js
@@ -1,1 +1,6 @@
+*/ Creating the title index
+db.books.createIndex({ title: 1 });
 
+
+*/ creating the author and publication year compund index
+db.books.createIndex({ author: 1, published_year: 1 });
